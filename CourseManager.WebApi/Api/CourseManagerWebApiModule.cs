249f418be8a4ec0c559113e5fbbd32ea9c5ecb08
mq <jeryhu1029@126.com>
2017-06-21 17:00:21 +0800

    [集成SwaggerApi文档 apis/index访问]

--- CourseManager.WebApi/Api/CourseManagerWebApiModule.cs
+++ CourseManager.WebApi/Api/CourseManagerWebApiModule.cs
@@ -5,7 +5,10 @@ using Abp.Configuration.Startup;
 using Abp.Modules;
 using Abp.WebApi;
 using CourseManager.Students;
-
+using Swashbuckle.Application;
+using System;
+using System.IO;
+using System.Linq;
 namespace CourseManager.Api
 {
     [DependsOn(typeof(AbpWebApiModule), typeof(CourseManagerApplicationModule))]
@@ -22,6 +25,28 @@ namespace CourseManager.Api
             //    .For<IStudentAppService>("app/student")
             //    .Build();
             Configuration.Modules.AbpWebApi().HttpConfiguration.Filters.Add(new HostAuthenticationFilter("Bearer"));
+            ConfigureSwaggerUi();
+        }
+
+        private void ConfigureSwaggerUi() ///swagger/ui/index apis/index
+        {
+            Configuration.Modules.AbpWebApi().HttpConfiguration
+               .EnableSwagger(c =>
+               {
+                   c.SingleApiVersion("v1", "CourseManager Api文档");
+                   c.ResolveConflictingActions(apiDescriptions => apiDescriptions.First());
+
+                   var baseDirectory = AppDomain.CurrentDomain.BaseDirectory;  //将application层中的注释添加到SwaggerUI中
+                   var commentsFileName = "Bin//CourseManager.Application.xml";
+                   var commentsFile = Path.Combine(baseDirectory, commentsFileName);
+                   c.IncludeXmlComments(commentsFile);  //将注释的XML文档添加到SwaggerUI中
+               })
+               .EnableSwaggerUi("apis/{*assetPath}", b =>
+               {
+                   //对js进行了拓展
+                   b.InjectJavaScript(Assembly.GetExecutingAssembly(), "CourseManager.SwaggerUi.scripts.swagger.js");
+               });
+            //.EnableSwaggerUi();
         }
     }
 }
